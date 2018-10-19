c0fa91efc4a22fcab6b048997ac5f85b62834598
Benoit Gayet <benoit.gayet@neuf.fr>
2018-10-19 23:00:24 +0200

    Initialisation

---
+++ GestionArbitres/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestionArbitres.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
